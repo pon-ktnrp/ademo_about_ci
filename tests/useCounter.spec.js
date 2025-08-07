f3bd95c1fe91154872a44b7d47f1dc5ca62d6984
pon-ktnrp <pon.ktnrp@gmail.com>
2025-08-08 03:47:31 +0700

    newTestcasestillFail

--- tests/useCounter.spec.js
+++ tests/useCounter.spec.js
@@ -1,5 +1,5 @@
-import { renderHook, act } from '@testing-library/react-hooks';
-import useCounter from '../../src/hooks/features/homepage/useCounter';
+import { renderHook, act } from '@testing-library/react';
+import useCounter from '../src/hooks/features/homepage/useCounter';
 
 describe('useCounter', () => {
   it('should initialize count to 0 and val to 1', () => {
